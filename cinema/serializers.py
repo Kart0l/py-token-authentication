5020ef02a2d9a0e29920da0aa12c0bf37be6bfa9
Oleksii Kartashov <oleksii.kartashov1@gmail.com>
2025-05-01 18:09:40 +0300

    flake2

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -96,9 +96,9 @@ class MovieSessionDetailSerializer(MovieSessionSerializer):
 class TicketSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         """
-        Validate ticket data using the static validate_ticket method from Ticket model.
-        This method checks if row and seat numbers are within the cinema hall's
-        capacity.
+        Validate ticket data using the static validate_ticket method from Ticket
+        model. This method checks if row and seat numbers are within the cinema
+        hall's capacity.
         """
         data = super(TicketSerializer, self).validate(attrs)
         row = attrs.get("row")
