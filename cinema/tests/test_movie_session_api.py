1391fad4c1515a65c34a679c40c35109af82f0e6
Oleksii Kartashov <oleksii.kartashov1@gmail.com>
2025-05-01 17:42:26 +0300

    Fix import problem

--- cinema/tests/test_movie_session_api.py
+++ cinema/tests/test_movie_session_api.py
@@ -9,7 +9,7 @@ from rest_framework import status
 from cinema.models import MovieSession, Movie, CinemaHall
 from cinema.tests.test_actor_api import sample_actor
 from cinema.tests.test_cinema_hall_api import sample_cinema_hall
-from cinema.tests.test_genre_api import sample_genres
+from cinema.tests.test_genre_api import sample_genre
 from cinema.tests.test_movie_api import sample_movie
 from user.tests.test_user_api import create_user
 from cinema.serializers import MovieSessionSerializer
